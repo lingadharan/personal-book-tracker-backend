286c891c03c908250349fdb531d2818cffd939f7
lingadharan <jlingadharan@gmail.com>
2026-07-12 15:47:17 +0530

    Worked on Athentication and Authorization process.

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 import BookController from '../controller/bookcontroller.js';
 import UserController from '../controller/userController.js';
+import {
+  googleCallback,
+  logoutUser,
+  redirectToGoogle,
+  verifyUser,
+} from '../auth/auth.js';
 
 const router = Router();
 const bookController = new BookController();
@@ -18,9 +24,11 @@ router.delete('/delete-book', (req, res) =>
   bookController.deleteBookDetails(req, res)
 );
 
-// User
-router.post('/create-user', (req, res) =>
-  userController.createUserDetails(req, res)
-);
+router.get('/auth/google', (req, res) => redirectToGoogle(req, res));
+router.get('/auth/google/callback', (req, res) => googleCallback(req, res));
+
+router.get('/auth/me', (req, res) => verifyUser(req, res));
+
+router.post('/auth/logout', (req, res) => logoutUser(req, res));
 
 export default router;
